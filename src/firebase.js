0263ee1d179b77ab407fcd4b5125c33e7ce2b6e7
Deston Bui <khangduy158@gmail.com>
2023-09-05 01:42:53 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,9 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYr7GmvZLFNqDpRQprB1qJT3jqD0lHO0c",
-  authDomain: "project-management-5f250.firebaseapp.com",
-  projectId: "project-management-5f250",
-  storageBucket: "project-management-5f250.appspot.com",
-  messagingSenderId: "504172795267",
-  appId: "1:504172795267:web:25f1ec8362e21e2e96d2b6",
-  measurementId: "G-J2LK6MMDWD",
+  //
+  //
+  //
 };
 
 // Initialize Firebase
